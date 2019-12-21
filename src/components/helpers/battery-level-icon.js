c34e64fc8dd8d4133d7e69a77daf1e4ed9250bfb
Samuel Reed <samuel.trace.reed@gmail.com>
2019-12-21 13:55:01 -0500

    refactor(battery): simpler battery icon logic

--- src/components/helpers/battery-level-icon.js
+++ src/components/helpers/battery-level-icon.js
@@ -6,23 +6,18 @@ import {
   faBatteryThreeQuarters
 } from '@fortawesome/free-solid-svg-icons'
 
-export default level => {
-  let battery
-  switch (true) {
-    case 0 <= level && level <= 9:
-      battery = { type: faBatteryEmpty, color: '#cc0001' }
-      break
-    case 10 <= level && level <= 39:
-      battery = { type: faBatteryQuarter, color: '#cc0001' }
-      break
-    case 40 <= level && level <= 69:
-      battery = { type: faBatteryHalf, color: '#1BC47D' }
-      break
-    case 70 <= level && level <= 90:
-      battery = { type: faBatteryThreeQuarters, color: '#1BC47D' }
-      break
-    default:
-      battery = { type: faBatteryFull, color: '#1BC47D' }
+const levels = {
+  10: { type: faBatteryEmpty, color: '#cc0001' },
+  40: { type: faBatteryQuarter, color: '#cc0001' },
+  70: { type: faBatteryHalf, color: '#1BC47D' },
+  90: { type: faBatteryThreeQuarters, color: '#1BC47D' },
+  100: { type: faBatteryFull, color: '#1BC47D' },
+}
+
+export default function batteryLevelIcon(thisLevel) {
+  for (const levelThresh of Object.keys(levels)) {
+    if (thisLevel <= Number(levelThresh)) return levels[levelThresh]
   }
-  return battery
+  // Over 100?
+  return levels[100]
 }
